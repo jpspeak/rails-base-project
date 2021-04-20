ab6c93ce5259de86dddd0d45378bf5a4774ab1c5
Angelo <goalista@gmail.com>
2021-04-20 22:35:32 +0800

    use dotenv variables

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,5 @@
 # Load the Rails application.
-require_relative 'application'
-
-# Load the app's custom environment variables here, before environments/*.rb
-app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
-load(app_env_vars) if File.exists?(app_env_vars)
+require_relative 'application
 
 # Initialize the Rails application.
 Rails.application.initialize!
