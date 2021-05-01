340d64cdd9e137015aea6d76fc4ca6586c9d1b59
Jay Philip <jayphilipcamillo@gmail.com>
2021-05-01 17:04:00 +0800

    Adds description to rpec model test

--- spec/model/buyers_stock_spec.rb
+++ spec/model/buyers_stock_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe BuyersStock, type: :model do
-  it { expect(subject).to belong_to(:buyer) }
-  it { expect(subject).to belong_to(:stock) }
+  describe 'ActiveRecord associations' do
+    it { expect(subject).to belong_to(:buyer) }
+    it { expect(subject).to belong_to(:stock) }
+  end
 end
