97dfe9c65ad5caad92cbc8f59c207dda0365331b
Angelo <goalista@gmail.com>
2021-05-04 17:03:38 +0800

    broker_spec rubocop corrections

--- spec/system/broker_spec.rb
+++ spec/system/broker_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
-describe "broker creation and features", type: :system do
-  it "registers" do
+describe 'broker creation and features', type: :system do
+  it 'registers' do
     visit new_broker_registration_path
     fill_in('Email', with: 'user2@example.com')
     fill_in('Password', with: 'password')
@@ -8,8 +8,8 @@ describe "broker creation and features", type: :system do
     click_on('Sign up')
   end
 
-  it "signs in and add stock" do
-    create(:broker,:confirmed,:approved)
+  it 'signs in and add stock' do
+    create(:broker, :confirmed, :approved)
     visit new_broker_session_path
     fill_in('Email', with: 'broker1@gmail.com')
     fill_in('Password', with: '123456')
